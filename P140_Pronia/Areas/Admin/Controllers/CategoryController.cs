8fc9d770753d8212fb1e1e236ff8842fa4880400
Javid-C <javidia@code.edu.az>
2023-12-28 12:57:07 +0400

    CRUD

---
+++ P140_Pronia/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P140_Pronia.Areas.Admin.ViewModels;
+using P140_Pronia.DAL;
+using P140_Pronia.Entities;
+
+namespace P140_Pronia.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ProniaDbContext _context;
+
+        public CategoryController(ProniaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories.AsEnumerable();
+
+            return View(categories);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            if (id == 0) return BadRequest();
+
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category is null) return NotFound();
+
+            return View(category);
+        }
+
+        //[HttpGet]
+        //[HttpPost]
+        //[HttpPut]
+        //[HttpPatch]
+        //[HttpDelete]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryVM category)
+        {
+            //var categoryName = Request.Form["category"];
+
+            if (!ModelState.IsValid) return View();
+
+            bool isExisted = _context.Categories.Any(c => c.Name == category.Name);
+
+            if (isExisted) return View();
+
+            Category newCategory = new Category
+            {
+                Name = category.Name
+            };
+
+            _context.Categories.Add(newCategory);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ P140_Pronia/Areas/Admin/ViewModels/CreateCategoryVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P140_Pronia.Areas.Admin.ViewModels
+{
+    public class CreateCategoryVM
+    {
+        [Required(ErrorMessage ="Zehmet olmasa doldurun")]
+        //[StringLength(maximumLength: 3)]
+        public string Name { get; set; } = null!;
+    }
+}
